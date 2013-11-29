ce25aa98afc3e75faf49ae612d311f76bd6e4f84
robertkruis <robertkruis>
2013-11-29 11:28:45 +0000

    Added basic implementation of the ClaimsTransformationManager to transform incoming claims to bioshock data.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/claims-transformation@3261 e4814794-f0f8-11e1-949a-37c09b20bee5

---
+++ Lisa.Bioshock/Modules/ClaimsTransformationManager.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Security.Claims;
+
+namespace Lisa.Bioshock.Modules
+{
+    public class ClaimsTransformationManager : ClaimsAuthenticationManager
+    {
+        public ClaimsTransformationManager()
+            : base()
+        {            
+        }
+
+        public override ClaimsPrincipal Authenticate(string resourceName, 
+            ClaimsPrincipal incomingPrincipal)
+        {
+            return base.Authenticate(resourceName, incomingPrincipal);
+        }
+    }
+}
