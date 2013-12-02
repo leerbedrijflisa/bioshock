d840060e6fed38d3e30d1c9a551813a51363162f
robertkruis <robertkruis>
2013-12-02 11:07:17 +0000

    Added UserController. Added view for the UserController.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/claims-transformation@3279 e4814794-f0f8-11e1-949a-37c09b20bee5

---
+++ Lisa.Bioshock/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lisa.Bioshock.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
