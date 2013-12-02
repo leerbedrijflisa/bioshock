96332bc6b8e85ba095ab01d68d57ebf04ec1feec
robertkruis <robertkruis>
2013-12-02 12:36:01 +0000

    Added action to log off from bioshock.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/claims-transformation@3286 e4814794-f0f8-11e1-949a-37c09b20bee5

--- Lisa.Bioshock/Controllers/UserController.cs
+++ Lisa.Bioshock/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Services;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,6 +18,17 @@ namespace Lisa.Bioshock.Controllers
         public ActionResult Login()
         {
             return RedirectToAction("Index");
+        }
+                
+        public ActionResult Logoff()
+        {
+            FederatedAuthentication.WSFederationAuthenticationModule.SignOut(false);
+
+            var uri = new Uri(FederatedAuthentication.WSFederationAuthenticationModule.Issuer);
+            var reply = FederatedAuthentication.WSFederationAuthenticationModule.Realm;
+            var signOutRequest = new SignOutRequestMessage(uri, reply);
+
+            return new RedirectResult(signOutRequest.WriteQueryString());
         }
     }
 }
