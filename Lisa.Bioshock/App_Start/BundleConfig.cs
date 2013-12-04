e1884c94c0774b84cc99317e8649a24261e4ab87
robertkruis <robertkruis>
2013-12-04 10:17:39 +0000

    Added bundle configuration with all bioshock related scripts, located in ~/scripts/bioshock and subdirectories. Added rendering of bioshock bundle to the _Layout.cshtml.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/fullscreen@3331 e4814794-f0f8-11e1-949a-37c09b20bee5

--- Lisa.Bioshock/App_Start/BundleConfig.cs
+++ Lisa.Bioshock/App_Start/BundleConfig.cs
@@ -38,6 +38,9 @@ namespace Lisa.Bioshock
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bioshock")
+                .IncludeDirectory("~/Scripts/bioshock", "*.js", true));
         }
     }
 }
