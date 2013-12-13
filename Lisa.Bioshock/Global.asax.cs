5bfbe759648882dcf06228d6a86f6019e1b18cd6
robertkruis <robertkruis>
2013-12-13 13:47:05 +0000

    Added sliding expiration by registering the SessionSecurityTokenReceived event with the handler SessionAuthenticationModule_SessionSecurityTokenReceived.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/trunk@3399 e4814794-f0f8-11e1-949a-37c09b20bee5

--- Lisa.Bioshock/Global.asax.cs
+++ Lisa.Bioshock/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.IdentityModel.Services;
+using System.IdentityModel.Tokens;
 
 namespace Lisa.Bioshock
 {
@@ -23,7 +25,34 @@ namespace Lisa.Bioshock
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);            
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            FederatedAuthentication.SessionAuthenticationModule.SessionSecurityTokenReceived +=
+                SessionAuthenticationModule_SessionSecurityTokenReceived;
+        }
+
+        protected void SessionAuthenticationModule_SessionSecurityTokenReceived(object sender,
+            SessionSecurityTokenReceivedEventArgs e)
+        {
+            var sessionToken = e.SessionToken;
+            var utcNow = DateTime.UtcNow;
+
+            if (sessionToken.ValidTo > utcNow)
+            {
+                // Create new token to extend the timeout by adding 20 seconds to the max timeout
+                // utcNow.AddSeconds(20) at every request made.
+                e.SessionToken = new SessionSecurityToken(
+                    sessionToken.ClaimsPrincipal,
+                    sessionToken.Context,
+                    sessionToken.ValidFrom,
+                    utcNow.AddSeconds(20));
+
+                e.ReissueCookie = true;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Token Expired");
+            }
         }
     }
 }
