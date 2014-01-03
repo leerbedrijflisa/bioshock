11483fa8f29cd7c2efbf3ab776042eacf13edf86
michaelschattgen <michaelschattgen>
2014-01-03 14:04:11 +0000

    Added EditorController.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/trunk@3451 e4814794-f0f8-11e1-949a-37c09b20bee5

---
+++ Lisa.Bioshock/Controllers/EditorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lisa.Bioshock.Controllers
+{
+    public class EditorController : Controller
+    {
+        //
+        // GET: /Editor/
+
+        public ActionResult Index()
+        {
+           
+            //Request.QueryString["project"];
+
+            return View();
+        }
+
+    }
+}
