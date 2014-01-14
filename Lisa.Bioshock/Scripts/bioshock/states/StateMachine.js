1dbf5b9e05abd4c8a41712a5e06566d0227f6517
robertkruis <robertkruis>
2014-01-14 11:27:49 +0000

    Added documentation to the StateMachine.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/refactor@3529 e4814794-f0f8-11e1-949a-37c09b20bee5

--- Lisa.Bioshock/Scripts/bioshock/states/StateMachine.js
+++ Lisa.Bioshock/Scripts/bioshock/states/StateMachine.js
@@ -1,10 +1,37 @@
+/// <reference path="IState.ts" />
 var StateMachine = (function () {
+    /**
+    * Creates a new instance of the StateMachine.
+    *
+    * @param {IState} defaultState - The first state to be enabled.
+    */
     function StateMachine(defaultState) {
         // fields
         this._stack = [];
         this._initialized = false;
         this.initialize(defaultState);
     }
+    /**
+    * Initializes the StateMachine.
+    *
+    * @param {IState} state - The first state to be enabled.
+    */
+    StateMachine.prototype.initialize = function (state) {
+        if (this._initialized) {
+            throw new Error('Already initialized!');
+        } else {
+            var preview = $("#preview")[0];
+            this._preview = preview.contentDocument || preview.contentWindow;
+            this.switchState(state);
+            this._initialized = true;
+        }
+    };
+
+    /**
+    * Switch to a new state and leaves the current one.
+    *
+    * @param {IState} state - The new state to switch to.
+    */
     StateMachine.prototype.switchState = function (state) {
         state.stateMachine = this;
 
@@ -19,8 +46,12 @@ var StateMachine = (function () {
         this.currentState.resume();
     };
 
+    /**
+    * Adds a state to the stack and suspends, but not leaves, the current one.
+    *
+    * @param {IState} state - The new state to push.
+    */
     StateMachine.prototype.pushState = function (state) {
-        console.log('pushing state: ' + state);
         state.stateMachine = this;
 
         if (this._stack.length > 0) {
@@ -32,9 +63,10 @@ var StateMachine = (function () {
         this.currentState.resume();
     };
 
+    /**
+    * Pops the current state from the stack and suspends and leaves it. The previous state will be resumed.
+    */
     StateMachine.prototype.popState = function () {
-        console.log('popping state: ' + this.currentState);
-
         var current = this.currentState;
 
         this._stack.pop();
@@ -44,19 +76,10 @@ var StateMachine = (function () {
         this.currentState.resume();
     };
 
-    StateMachine.prototype.initialize = function (state) {
-        if (this._initialized) {
-            throw new Error('Already initialized!');
-        } else {
-            var preview = $("#preview")[0];
-            this._preview = preview.contentDocument || preview.contentWindow;
-            this.switchState(state);
-            this._initialized = true;
-        }
-    };
-
     Object.defineProperty(StateMachine.prototype, "preview", {
-        // properties
+        /**
+        * Gets the contentDocument/contentWindow of the preview's IFrame.
+        */
         get: function () {
             return this._preview;
         },
@@ -65,6 +88,9 @@ var StateMachine = (function () {
     });
 
     Object.defineProperty(StateMachine.prototype, "currentState", {
+        /**
+        * Gets the current state.
+        */
         get: function () {
             return this._stack[this._stack.length - 1];
         },
--- Lisa.Bioshock/Scripts/bioshock/states/StateMachine.ts
+++ Lisa.Bioshock/Scripts/bioshock/states/StateMachine.ts
@@ -1,10 +1,39 @@
+/// <reference path="IState.ts" />
 class StateMachine {
 
+    /**
+     * Creates a new instance of the StateMachine.
+     *
+     * @param {IState} defaultState - The first state to be enabled.
+     */
     constructor(defaultState: IState) {
 
         this.initialize(defaultState);
     }
 
+    /**
+     * Initializes the StateMachine.
+     *
+     * @param {IState} state - The first state to be enabled.
+     */
+    public initialize(state: IState) {
+        if (this._initialized) {
+
+            throw new Error('Already initialized!');
+        } else {
+            var preview = <HTMLIFrameElement>$("#preview")[0];
+            this._preview = preview.contentDocument || preview.contentWindow;
+            this.switchState(state);
+            this._initialized = true;
+        }
+    }
+
+    
+    /**
+     * Switch to a new state and leaves the current one.
+     *
+     * @param {IState} state - The new state to switch to.
+     */
     public switchState(state: IState) {
         state.stateMachine = this;
 
@@ -20,6 +49,11 @@ class StateMachine {
         this.currentState.resume();
     }
 
+    /**
+     * Adds a state to the stack and suspends, but not leaves, the current one.
+     *
+     * @param {IState} state - The new state to push.
+     */
     public pushState(state: IState) {
         state.stateMachine = this;
 
@@ -33,8 +67,10 @@ class StateMachine {
         this.currentState.resume();
     }
 
+    /**
+     * Pops the current state from the stack and suspends and leaves it. The previous state will be resumed.
+     */
     public popState() {
-
         var current = this.currentState;
 
         this._stack.pop();
@@ -44,25 +80,18 @@ class StateMachine {
         this.currentState.resume();
     }
 
-    private initialize(state: IState) {
-
-        if (this._initialized) {
-
-            throw new Error('Already initialized!');
-        } else {
-            var preview = <HTMLIFrameElement>$("#preview")[0];
-            this._preview = preview.contentDocument || preview.contentWindow;
-            this.switchState(state);
-            this._initialized = true;
-        }
-    }
 
 
-    // properties
-    public get preview(): HTMLIFrameElement {
+    /**
+     * Gets the contentDocument/contentWindow of the preview's IFrame.
+     */
+    public get preview() {
         return this._preview;
     }
 
+    /**
+     * Gets the current state.
+     */
     public get currentState(): IState {
         return this._stack[this._stack.length - 1];
     }
