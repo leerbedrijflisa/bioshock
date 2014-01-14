c0144df4eb8197b8c4713c18656eedc02764ad80
robertkruis <robertkruis>
2014-01-14 11:22:36 +0000

    Renamed Workspace to BioshockWorkspace. BioshockWorkspace is not a singleton anymore. Initialized the global Workspace object.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/refactor@3526 e4814794-f0f8-11e1-949a-37c09b20bee5

--- Lisa.Bioshock/Scripts/bioshock/Workspace.js
+++ Lisa.Bioshock/Scripts/bioshock/Workspace.js
@@ -1,67 +1,24 @@
-var Workspace = (function () {
-    function Workspace() {
-        this._windows = {};
-        if (Workspace._instance !== undefined) {
-            throw new Error("Instance already defined!");
-        }
-
-        this.projectID = $("#ProjectID").val();
-        this.ajax = new AjaxHelper(this.projectID);
+var BioshockWorkspace = (function () {
+    /**
+    * Creates a new BioshockWorkspace.
+    * @param {string} projectID - This ID is being used by UIWindows and ajax requests.
+    */
+    function BioshockWorkspace(projectID) {
+        this.projectID = projectID;
+        this._ajax = new AjaxHelper(projectID);
         this.stateMachine = new StateMachine(new PreviewState());
-
-        Workspace._instance = this;
     }
-    Object.defineProperty(Workspace, "instance", {
+    Object.defineProperty(BioshockWorkspace.prototype, "ajax", {
+        /**
+        * Gets the AjaxHelper instance.
+        */
         get: function () {
-            if (Workspace._instance === undefined) {
-                Workspace._instance = new Workspace();
-            }
-
-            return Workspace._instance;
+            return this._ajax;
         },
         enumerable: true,
         configurable: true
     });
-
-    Object.defineProperty(Workspace.prototype, "newFileWindow", {
-        // UIWindow Getters \\
-        //public get editorWindow(): EditorWindow {
-        //    if (this._windows["editor"] === undefined) {
-        //        this._windows["editor"] = new EditorWindow("#editorWindow");
-        //    }
-        //    return this._windows["editor"];
-        //}
-        get: function () {
-            if (this._windows["newFile"] === undefined) {
-                this._windows["newFile"] = new NewFileWindow("#newFileWindow");
-            }
-            return this._windows["newFile"];
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Workspace.prototype, "openFileWindow", {
-        get: function () {
-            if (this._windows["openFile"] === undefined) {
-                this._windows["openFile"] = new OpenFileWindow("#openFileWindow");
-            }
-            return this._windows["openFile"];
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Workspace.prototype, "menuWindow", {
-        get: function () {
-            if (this._windows["menu"] === undefined) {
-                this._windows["menu"] = new MenuWindow('#editorMenuWindow');
-            }
-            return this._windows["menu"];
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return Workspace;
+    return BioshockWorkspace;
 })();
+var Workspace;
 //# sourceMappingURL=Workspace.js.map
--- Lisa.Bioshock/Scripts/bioshock/Workspace.ts
+++ Lisa.Bioshock/Scripts/bioshock/Workspace.ts
@@ -1,61 +1,25 @@
-class Workspace {   
+class BioshockWorkspace {
 
-    constructor() {
-        if (Workspace._instance !== undefined) {
-            throw new Error("Instance already defined!");
-        }
-
-        this.projectID = $("#ProjectID").val();
-        this.ajax = new AjaxHelper(this.projectID);
+    /**
+     * Creates a new BioshockWorkspace.
+     * @param {string} projectID - This ID is being used by UIWindows and ajax requests.
+     */
+    constructor(projectID: string) {
+        this.projectID = projectID;
+        this._ajax = new AjaxHelper(projectID);
         this.stateMachine = new StateMachine(new PreviewState());
-
-        Workspace._instance = this;
     }
 
-    public static get instance() {
-        if (Workspace._instance === undefined) {
-            Workspace._instance = new Workspace();
-        }
-
-        return Workspace._instance;
+    /**
+     * Gets the AjaxHelper instance.
+     */
+    public get ajax(): AjaxHelper {
+        return this._ajax;
     }
 
-
-    // UIWindow Getters \\
-    //public get editorWindow(): EditorWindow {
-    //    if (this._windows["editor"] === undefined) {
-    //        this._windows["editor"] = new EditorWindow("#editorWindow");
-    //    }
-    //    return this._windows["editor"];
-    //}
-
-    public get newFileWindow(): NewFileWindow {
-        if (this._windows["newFile"] === undefined) {
-            this._windows["newFile"] = new NewFileWindow("#newFileWindow");
-        }
-        return this._windows["newFile"];
-    }
-
-    public get openFileWindow(): OpenFileWindow {
-        if (this._windows["openFile"] === undefined) {
-            this._windows["openFile"] = new OpenFileWindow("#openFileWindow");
-        }
-        return this._windows["openFile"];
-    }
-
-    public get menuWindow(): MenuWindow {
-        if (this._windows["menu"] === undefined) {
-            this._windows["menu"] = new MenuWindow('#editorMenuWindow');
-        }
-        return this._windows["menu"];
-    }
-    // End UIWindow Getters \\
-
-
-    public ajax: AjaxHelper;
-    public projectID: any;
+    public projectID: string;
     public stateMachine: StateMachine;
 
-    private static _instance: Workspace;
-    private _windows: Object = {};
+    private _ajax: AjaxHelper;
 }
+var Workspace: BioshockWorkspace;
