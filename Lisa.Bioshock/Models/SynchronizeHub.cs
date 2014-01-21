93b81065f9eaa4197060482c8cd053161a2acc58
robertkruis <robertkruis>
2014-01-21 11:38:34 +0000

    The file that is synchronized will be updated on the storage.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/refactor@3597 e4814794-f0f8-11e1-949a-37c09b20bee5

--- Lisa.Bioshock/Models/SynchronizeHub.cs
+++ Lisa.Bioshock/Models/SynchronizeHub.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Lisa.Bioshock.Data;
+using Lisa.Storage;
+using Lisa.Storage.Data;
+using Lisa.Bioshock.Extensions;
 
 namespace Lisa.Bioshock.Models
 {
@@ -17,7 +21,42 @@ namespace Lisa.Bioshock.Models
     {
         public void ProcessChanges(FileDescriptor file, string contents)
         {
+            WriteFile(file, contents);
+
             Clients.All.Update(file, contents);
+        }
+
+        private void WriteFile(FileDescriptor file, string contents)
+        {
+            using (var db = new BioshockContext())
+            {
+                var project = db.Projects.Find(file.ProjectID);
+                if (project != null)
+                {
+                    var fileSystem = CreateFileSystem(project.RootID);
+                    var currentFile = fileSystem.Root.FindItemByID(file.ID) as File;
+
+                    if (currentFile != null)
+                    {
+                        try
+                        {
+                            currentFile.WriteContents(contents);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.StackTrace);
+                        }
+                    }
+                }
+            }
+        }
+
+        private FileSystem CreateFileSystem(Guid rootID)
+        {
+            LocalStorageProvider provider = new LocalStorageProvider("/Storage/" + rootID);
+            FileSystem fileSystem = new FileSystem(provider);
+
+            return fileSystem;
         }
     }
 }
