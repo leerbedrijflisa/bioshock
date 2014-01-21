005338d481b6bacd4de16f3aa295d25f2a7591d5
robertkruis <robertkruis>
2014-01-21 19:27:32 +0000

    Added FileSystemHelper to ease the use of the FileSystem.
    git-svn-id: svn://mona.davinci.nl/lisa/bioshock/branches/refactor@3606 e4814794-f0f8-11e1-949a-37c09b20bee5

---
+++ Lisa.Bioshock/Helpers/FileSystemHelper.cs
@@ -0,0 +1,30 @@
+﻿using Lisa.Storage;
+using Lisa.Storage.Data.Web;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Lisa.Bioshock.Helpers
+{
+    public static class FileSystemHelper
+    {
+        public static FileSystem GetFileSystem(string rootID)
+        {
+            var provider = new CloudStorageProvider
+            (
+                ConfigurationManager.AppSettings["CloudStorageConnectionString"],
+                "bioshock",
+                rootID.ToString()
+            );
+
+            return new FileSystem(provider);
+        }
+
+        public static FileSystem GetFileSystem(Guid rootID)
+        {
+            return FileSystemHelper.GetFileSystem(rootID.ToString());
+        }
+    }
+}
